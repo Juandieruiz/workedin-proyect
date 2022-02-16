d874ec06a810cac90a8265fd05508170c41f063c
Juan Diego Gómez <77864382+Juandieruiz@users.noreply.github.com>
2022-02-16 19:25:39 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-firebaseConfig : {
+const firebaseConfig = {
   apiKey: "AIzaSyClM1foyX9stmSpChKJ5tPvbS6ejp-kULI",
   authDomain: "workedin-8d38f.firebaseapp.com",
   projectId: "workedin-8d38f",
@@ -8,6 +8,6 @@ firebaseConfig : {
   messagingSenderId: "261630294719",
   appId: "1:261630294719:web:891cacfbf84bf8aaa1e40a",
   measurementId: "G-MBNDRJHXJG"
-}
+};
 
 };
